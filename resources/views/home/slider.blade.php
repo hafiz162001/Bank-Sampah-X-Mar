b446c7962ad2d472b6105d1c10c0f22dd74b2f39
asa <hafizachmadr@gmail.com>
2024-03-04 13:14:07 +0700

    sanasa

---
+++ resources/views/home/slider.blade.php
@@ -0,0 +1,10 @@
+<div id="slider-rev-container">
+    <div id="slider-rev">
+        <ul>
+            <li data-transition="fade" data-saveperformance="on">
+                <img src="{{ asset('img/homeslider/p.jpg') }}" alt="background" style="background-color:#f1f6f7">
+            </li>
+           
+        </ul>
+    </div>{{-- End #slider-rev --}}
+</div>{{-- End #slider-rev-container --}}
